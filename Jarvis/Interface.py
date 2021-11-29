6a1fbe87a9f1cbd0514676a82313cbfa855e1484
DidonCode <76182682+DidonCode@users.noreply.github.com>
2021-11-29 21:42:14 +0100

    Delete Jarvis directory

--- Jarvis/Interface.py
+++
@@ -1,9 +0,0 @@
-from tkinter import * 
-
-def StartInterface():
-	fenetre = Tk()
-
-	fenetre.title("Jack Interface")
-	fenetre.geometry("1280x720")
-
-	fenetre.mainloop()
--- Jarvis/Start.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\Richard\Desktop\Jarvis
-start Main.py
